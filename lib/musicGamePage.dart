4a52e2489a56ed17e650e3bf26437e76e2b6f08e
emeli <gusfolkem@student.gu.se>
2021-04-21 15:17:28 +0200

    added initState
    Co-authored-by: EllinorStomdal <EllinorStomdal@users.noreply.github.com>

--- lib/musicGamePage.dart
+++ lib/musicGamePage.dart
@@ -2,15 +2,33 @@ import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-class MusicGamePage extends StatelessWidget {
-  static AudioCache cache = new AudioCache();
-  static AudioPlayer player = new AudioPlayer();
+class MusicGamePage extends StatefulWidget {
+  @override
+  _MusicGamePageState createState() => _MusicGamePageState();
+}
 
-  final water = "water.mp3";
+class _MusicGamePageState extends State<MusicGamePage> {
+  AudioCache cache;
+  final test = "Vatten.wav";
+
+  Future _playAll() async {
+    await cache.play(test);
+    //await cache.load(test);
+    cache.loop(test);
+  }
+
+  void _stopAll() {
+    //player?.stop();
+  }
 
   @override
+  void initState() {
+    super.initState();
+    cache = AudioCache();
+    cache.load(test);
+  }
+
   Widget build(BuildContext context) {
-    _playAll();
     return Scaffold(
       body: Center(
         child: Column(
@@ -24,14 +42,6 @@ class MusicGamePage extends StatelessWidget {
     ); //Scaffold
   }
 
-  void _playAll() async {
-    player = await cache.loop(water);
-  }
-
-  void _stopAll() {
-    player?.stop();
-  }
-
   Widget _image(context) {
     return Stack(
       children: [
@@ -41,7 +51,7 @@ class MusicGamePage extends StatelessWidget {
             child: IconButton(
               icon: Icon(Icons.arrow_back),
               onPressed: () {
-                _stopAll();
+                //_stopAll();
                 Navigator.pop(context);
               },
               color: Colors.orange,
@@ -52,10 +62,12 @@ class MusicGamePage extends StatelessWidget {
             child: IconButton(
               icon: Icon(Icons.music_note_rounded),
               iconSize: 100,
-              onPressed: () {},
+              onPressed: () {
+                _playAll();
+              },
               color: Colors.orange,
             )),
       ],
     );
-  }
-} //MainView
+  } //widget image
+}
