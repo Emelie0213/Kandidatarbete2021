57ea42cf75b37593e4161012262d0e1c2978cecc
emeli <gusfolkem@student.gu.se>
2021-04-22 11:57:32 +0200

    added working sound & startbutton
    Co-authored-by: EllinorStomdal <EllinorStomdal@users.noreply.github.com>

--- lib/musicGamePage.dart
+++ lib/musicGamePage.dart
@@ -1,6 +1,7 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class MusicGamePage extends StatefulWidget {
   @override
@@ -8,66 +9,86 @@ class MusicGamePage extends StatefulWidget {
 }
 
 class _MusicGamePageState extends State<MusicGamePage> {
-  AudioCache cache;
-  final test = "Vatten.wav";
+  AudioCache cache; // = AudioCache();
+  AudioPlayer player;
+  final test = "Flourlack.wav";
 
-  Future _playAll() async {
-    await cache.play(test);
-    //await cache.load(test);
-    cache.loop(test);
+  void _playAll() async {
+    await cache.load(test);
+    player = await cache.loop(test, volume: 0.0);
   }
 
   void _stopAll() {
-    //player?.stop();
+    player?.stop();
   }
 
   @override
   void initState() {
     super.initState();
     cache = AudioCache();
-    cache.load(test);
+    //cache.load(test);
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _image(context),
-          ],
-        ), //Column
-      ), //Center
-    ); //Scaffold
-  }
+          child: Stack(
+        children: [
+          _image(context),
+          _backButton(),
+          _startButton(),
+        ],
+      )),
+    );
+  } //build
 
   Widget _image(context) {
-    return Stack(
-      children: [
-        Image(image: AssetImage("assets/MusicGame.PNG")),
-        Positioned(
-            top: 10,
-            child: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                //_stopAll();
-                Navigator.pop(context);
-              },
-              color: Colors.orange,
-            )),
-        Positioned(
-            top: 170,
-            left: 180,
-            child: IconButton(
-              icon: Icon(Icons.music_note_rounded),
-              iconSize: 100,
+    return Image(image: AssetImage("assets/Ljudspelet.png"));
+  }
+
+  Widget _backButton() {
+    return Positioned(
+      top: 10,
+      child: Transform.rotate(
+        angle: 180 * math.pi / 180,
+        child: IconButton(
+          icon: Icon(Icons.forward),
+          iconSize: 50,
+          onPressed: () {
+            _stopAll();
+            Navigator.pop(context);
+          },
+          color: Color.fromRGBO(42, 132, 210, 1.0),
+        ),
+      ),
+    );
+  } //_backButton
+
+  Widget _startButton() {
+    return Positioned(
+      child: Center(
+        child: Container(
+          width: 120,
+          height: 120,
+          child: FloatingActionButton(
+              backgroundColor: Color.fromRGBO(42, 132, 210, 1.0),
+              shape: StadiumBorder(
+                  side: BorderSide(
+                color: Colors.blue[900],
+                width: 4,
+              )),
+              child: Text(
+                'START',
+                textScaleFactor: 1.5,
+              ),
               onPressed: () {
                 _playAll();
-              },
-              color: Colors.orange,
-            )),
-      ],
+              }),
+        ),
+      ),
     );
-  } //widget image
+  }
+
+  //_startButton
+
 }
