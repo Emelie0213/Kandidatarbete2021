4fe6877ec6dfdf08ae478660ef1f16b77788fc53
emeli <gusfolkem@student.gu.se>
2021-04-29 11:41:07 +0200

    added all buttons
    Co-authored-by: EllinorStomdal <EllinorStomdal@users.noreply.github.com>

--- lib/musicGamePage.dart
+++ lib/musicGamePage.dart
@@ -14,7 +14,16 @@ class _MusicGamePageState extends State<MusicGamePage> {
   bool visibleStartButton = true;
   bool visibleMusicButtons = false;
 
+  bool blasterPlaying = false;
+  bool borrPlaying = false;
   bool flourlackPlaying = false;
+  bool putsPlaying = false;
+  bool rontgenPlaying = false;
+  bool sickelPlaying = false;
+  bool sondPlaying = false;
+  bool stolPlaying = false;
+  bool sugPlaying = false;
+  bool vattenPlaying = false;
 
   @override
   void initState() {
@@ -41,8 +50,17 @@ class _MusicGamePageState extends State<MusicGamePage> {
         ),
         _image(context, data),
         _startButton(),
-        _backButton(),
+        _blasterButton(data),
+        _borrButton(data),
         _flourlackButton(data),
+        _putsButton(data),
+        _rontgenButton(data),
+        _sickelButton(data),
+        _sondButton(data),
+        _stolButton(data),
+        _sugButton(data),
+        _vattenButton(data),
+        _backButton(),
       ],
     ));
   } //build
@@ -110,25 +128,80 @@ class _MusicGamePageState extends State<MusicGamePage> {
     );
   } //_backButton
 
+  Widget _blasterButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: blasterPlaying,
+        whenNotPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Blaster_OFF.png',
+          left: data.size.height * 1.02,
+          top: data.size.width * 0.17,
+          rotation: 38,
+          width: 36,
+          height: 100.5,
+        ),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Blaster_ON.png',
+          left: data.size.height * 1.02,
+          top: data.size.width * 0.17,
+          rotation: 38,
+          width: 36,
+          height: 100.5,
+        ),
+        onPressed: () {
+          setState(() {
+            blasterPlaying = !blasterPlaying;
+            print('you pressed blaster!');
+          });
+        });
+  } //blasterButton
+
+  Widget _borrButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: borrPlaying,
+        whenNotPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Borr_OFF.png',
+          left: data.size.height * 1.13,
+          top: data.size.width * 0.185,
+          rotation: 36,
+          width: 31.1,
+          height: 94,
+        ),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Borr_ON.png',
+          left: data.size.height * 1.13,
+          top: data.size.width * 0.185,
+          rotation: 36,
+          width: 31.1,
+          height: 94,
+        ),
+        onPressed: () {
+          setState(() {
+            borrPlaying = !borrPlaying;
+            print('you pressed borr!');
+          });
+        });
+  } //borrButton
+
   Widget _flourlackButton(data) {
-    print('${data.size.height}');
     return ImageButtonWidget(
         isVisible: visibleMusicButtons,
         isPlaying: flourlackPlaying,
         whenNotPlaying: ImageButtonInfo(
             filename: 'assets/Images/Flourlack_OFF.png',
             left: data.size.height * 0.95,
-            top: data.size.width * 0.36,
+            top: data.size.width * 0.34,
             rotation: 55,
-            width: 24.28,
-            height: 60.78),
+            width: 29.37,
+            height: 73.44),
         whenPlaying: ImageButtonInfo(
           filename: 'assets/Images/Flourlack_ON.png',
           left: data.size.height * 0.95,
-          top: data.size.width * 0.36,
-          rotation: 50,
-          width: 28.5,
-          height: 66,
+          top: data.size.width * 0.34,
+          rotation: 55,
+          width: 29.37,
+          height: 73.44,
         ),
         onPressed: () {
           setState(() {
@@ -137,60 +210,195 @@ class _MusicGamePageState extends State<MusicGamePage> {
           });
         });
   } //FlourlackButton
-} //MusicGamePageState
 
-/* if (_florlackMuted == true) {
-      return Positioned(
-        top: 200,
-        left: 340,
-        child: Visibility(
-          visible: visibleMusicButtons,
-          child: InkWell(
-            onTap: () {
-              setState(() {
-                print("you tapped flourlackButton");
-                //audioList.elementAt(0).unmute();
-                _florlackMuted = false;
-              });
-            },
-            child: RotationTransition(
-              turns: AlwaysStoppedAnimation(55 / 360),
-              child: Image(
-                image: AssetImage('assets/Images/Flourlack_OFF.png'),
-                height: 60.78,
-                width: 24.28,
-              ),
-            ),
-          ),
+  Widget _putsButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: putsPlaying,
+        whenNotPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Puts_OFF.png',
+          left: data.size.height * 1.24,
+          top: data.size.width * 0.21,
+          rotation: 34,
+          width: 27,
+          height: 92,
         ),
-      );
-    } else {
-      return Positioned(
-        top: 200,
-        left: 340,
-        child: Visibility(
-          visible: visibleMusicButtons,
-          child: InkWell(
-            onTap: () {
-              setState(
-                () {
-                  print("you tapped flourlackButton");
-                  //audioList.elementAt(0).unmute();
-                  _florlackMuted = true;
-                },
-              );
-            },
-            child: RotationTransition(
-              turns: AlwaysStoppedAnimation(55 / 360),
-              child: Image(
-                image: AssetImage('assets/Images/Flourlack_ON.png'),
-                height: 60.78,
-                width: 24.28,
-              ),
-            ),
-          ),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Puts_ON.png',
+          left: data.size.height * 1.24,
+          top: data.size.width * 0.21,
+          rotation: 34,
+          width: 27,
+          height: 92,
         ),
-      );
-    }
-  } //_flourlackButton
-}*/
+        onPressed: () {
+          setState(() {
+            putsPlaying = !putsPlaying;
+            print('you pressed borr!');
+          });
+        });
+  } //putsButton
+
+  Widget _rontgenButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: rontgenPlaying,
+        whenNotPlaying: ImageButtonInfo(
+            filename: 'assets/Images/Rontgen_OFF.png',
+            left: data.size.height * -0.04,
+            top: data.size.width * 0.016,
+            rotation: 0,
+            width: 224,
+            height: 138),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Rontgen_ON.png',
+          left: data.size.height * -0.04,
+          top: data.size.width * 0.016,
+          rotation: 0,
+          width: 224,
+          height: 138,
+        ),
+        onPressed: () {
+          setState(() {
+            rontgenPlaying = !rontgenPlaying;
+            print('you pressed rontgen!');
+          });
+        });
+  } //rontgenButton
+
+  Widget _sickelButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: sickelPlaying,
+        whenNotPlaying: ImageButtonInfo(
+            filename: 'assets/Images/Sickel_OFF.png',
+            left: data.size.height * 1.26,
+            top: data.size.width * 0.41,
+            rotation: 48,
+            width: 29.88,
+            height: 92.8),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Sickel_ON.png',
+          left: data.size.height * 1.26,
+          top: data.size.width * 0.41,
+          rotation: 48,
+          width: 29.88,
+          height: 92.8,
+        ),
+        onPressed: () {
+          setState(() {
+            sickelPlaying = !sickelPlaying;
+            print('you pressed sickel!');
+          });
+        });
+  } //sickelButton
+
+  Widget _sondButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: sondPlaying,
+        whenNotPlaying: ImageButtonInfo(
+            filename: 'assets/Images/Sond_OFF.png',
+            left: data.size.height * 1.095,
+            top: data.size.width * 0.362,
+            rotation: 49,
+            width: 33.55,
+            height: 99),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Sond_ON.png',
+          left: data.size.height * 1.095,
+          top: data.size.width * 0.362,
+          rotation: 49,
+          width: 33.55,
+          height: 99,
+        ),
+        onPressed: () {
+          setState(() {
+            sondPlaying = !sondPlaying;
+            print('you pressed sond!');
+          });
+        });
+  } //sondButton
+
+  Widget _stolButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: stolPlaying,
+        whenNotPlaying: ImageButtonInfo(
+            filename: 'assets/Images/Stol_OFF.png',
+            left: data.size.height * 0.92,
+            top: data.size.width * 0.130,
+            rotation: 32,
+            width: 41.8,
+            height: 88.6),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Stol_ON.png',
+          left: data.size.height * 0.92,
+          top: data.size.width * 0.130,
+          rotation: 32,
+          width: 41.8,
+          height: 88.6,
+        ),
+        onPressed: () {
+          setState(() {
+            stolPlaying = !stolPlaying;
+            print('you pressed stol!');
+          });
+        });
+  } //stolButton
+
+  Widget _sugButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: sugPlaying,
+        whenNotPlaying: ImageButtonInfo(
+            filename: 'assets/Images/Sug_OFF.png',
+            left: data.size.height * 0.12,
+            top: data.size.width * 0.34,
+            rotation: 0,
+            width: 96,
+            height: 150),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Sug_ON.png',
+          left: data.size.height * 0.12,
+          top: data.size.width * 0.34,
+          rotation: 0,
+          width: 96,
+          height: 150,
+        ),
+        onPressed: () {
+          setState(() {
+            sugPlaying = !sugPlaying;
+            print('you pressed sug!');
+          });
+        });
+  } //sugButton
+
+  Widget _vattenButton(data) {
+    return ImageButtonWidget(
+        isVisible: visibleMusicButtons,
+        isPlaying: vattenPlaying,
+        whenNotPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Vatten_OFF.png',
+          left: data.size.height * 0.45,
+          top: data.size.width * 0.24,
+          rotation: 3,
+          width: 126.47,
+          height: 130,
+        ),
+        whenPlaying: ImageButtonInfo(
+          filename: 'assets/Images/Vatten_ON.png',
+          left: data.size.height * 0.45,
+          top: data.size.width * 0.24,
+          rotation: 3,
+          width: 126.47,
+          height: 130,
+        ),
+        onPressed: () {
+          setState(() {
+            vattenPlaying = !vattenPlaying;
+            print('you pressed vatten!');
+          });
+        });
+  } //vattenButton
+} //MusicGamePageState
